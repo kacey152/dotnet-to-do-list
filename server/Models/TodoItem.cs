1e2ddef68df1e9783af708d2e60fa93c052ff1b8
Kacey Chua <110165967+kacey152@users.noreply.github.com>
2024-03-05 19:29:06 +0800

    Fixed the auto generation of id

--- server/Models/TodoItem.cs
+++ server/Models/TodoItem.cs
@@ -1,11 +1,18 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
 namespace Server.Models
 {
     public class TodoItem
     {
-        public int Id {get; set;}
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
         public DateTime Date { get; set; }
         public string? Category { get; set; }
         public bool RemindEnabled { get; set; }
         public string? Description { get; set; }
     }
-}    
+}
